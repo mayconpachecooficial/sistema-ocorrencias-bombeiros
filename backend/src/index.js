93deb946dabb35ce77d37bfa2ee140a59909f0b8
MAYCON PACHECO <151109977+mayconpachecooficial@users.noreply.github.com>
2025-06-27 17:16:15 -0400

    update

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.json());
 app.use("/api/ocorrencias", ocorrencias);
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`Servidor na porta ${PORT}`));
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
 module.exports = app;
