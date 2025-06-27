fce35d82cd86efd875a6c351cb4cec9ec7220489
MAYCON PACHECO <151109977+mayconpachecooficial@users.noreply.github.com>
2025-06-27 18:08:48 -0400

    install cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const cors = require("cors"); 
+
 const ocorrencias = require("./routes/ocorrencias");
+
 const app = express();
+app.use(cors()); 
 app.use(express.json());
+
 app.use("/api/ocorrencias", ocorrencias);
+
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+app.listen(PORT, () => console.log(`Servidor na porta ${PORT}`));
+
 module.exports = app;
